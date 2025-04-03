2b3a258901ce1094201299a071e639d4aaf58517
remmouche <121981104+remmouche@users.noreply.github.com>
2025-04-03 01:24:07 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+// annotations
+var var1 = 'Samy';
+var1 = 'Maya';
+console.log(var1);
+//type inference
+var tech = "TypeScript";
---
+++ index.ts
@@ -0,0 +1,16 @@
+
+// annotations
+let var1: string = 'Samy';
+var1 = 'Maya';
+console.log(var1);
+
+//type inference
+let tech = "TypeScript";
+let favNumber = 7;
+let isHard= true;
+
+console.log(tech, favNumber, isHard);
+// ERROR
+// tech = 7;
+// favNumber = false;
+// isHard = 'true'
